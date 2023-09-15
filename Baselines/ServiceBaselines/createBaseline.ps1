f1430245f457af8df3f5b61e8f51466905672ad0
JohnDCode <112843671+JohnDCode@users.noreply.github.com>
2023-09-15 08:30:39 -0700

    Delete Baselines/ServiceBaselines directory

--- Baselines/ServiceBaselines/createBaseline.ps1
+++
@@ -1,4 +0,0 @@
-$services = Get-Service | Select-Object Name, DisplayName, Status, ServiceType, StartType, ProcessId, Description, DependentServices, ServicesDependedOn, MachineName, CanPauseAndContinue, CanShutdown, CanStop, Site, RequiredServices, CanHandlePowerEvent, CanHandleSessionChangeEvent, CanHandleSuspendResume, CanHandleSessionChange, ExitCode, StartName, SiteName, PathName, PagedMemorySize, PeakPagedMemorySize, PrivateMemorySize, PeakVirtualMemorySize, PeakWorkingSet, VirtualMemorySize, WorkingSet, StartTime
-
-
-$services | Export-Csv -Path ".\export.csv" -NoTypeInformation
