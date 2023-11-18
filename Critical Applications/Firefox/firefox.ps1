89768988a9eb1b53b0d16f345ba30ca7bda93549
JohnDCode <112843671+JohnDCode@users.noreply.github.com>
2023-11-17 16:02:41 -0800

    Add files via upload

---
+++ Critical Applications/Firefox/firefox.ps1
@@ -0,0 +1,30 @@
+# Created By: JohnDavid Abe
+# Created On: 11/16/23
+# Last Modified: 11/16/23
+# Title: firefox.ps1
+# Description: Modifies firefox preferences (specifically the prefs/prefs.js file) to secure Windows Firefox
+# Version: 1.0
+
+
+
+# Log the script running
+$date = Get-Date
+Add-Content .\log.txt "Script Ran: firefox.ps1"
+Add-Content .\log.txt "Date: $date"
+Add-Content .\log.txt ""
+
+# Save the number of actions taken
+$actionNumber = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
