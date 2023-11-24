19b94337922cb3f76ff0cfbcb291237f6cfa948f
unknown <johndavidabe101@gmail.com>
2023-11-24 02:43:51 -0800

    Updated filter

--- Baseline/filter.ps1
+++ Baseline/filter.ps1
@@ -1,7 +1,7 @@
 # Created By: JohnDavid Abe
 # Created On: 11/24/23
 # Last Modified: 11/24/23
-# Title: shields.ps1
+# Title: filter.ps1
 # Description: Used to filter output of baseline scripts
 # Version: 1.0
 
@@ -11,10 +11,17 @@
 
 $outputContent = Get-Content .\output.txt
 foreach($line in $outputContent) {
-	$signature = Get-AuthenticodeSignature -ErrorAction SilentlyContinue -FilePath $line
+	try {
+        $signature = Get-AuthenticodeSignature -FilePath $line
         $signer = $signature.SignerCertificate.Subject
 
-        if (-not($signer -like "*Microsoft Corporation*")) {
-		$line | Add-Content -Path .\Filter\filtered.txt
+        if ($signer -like "*Microsoft Corporation*") {
+            
+        } else {
+            $line | Add-Content -Path .\Filter\filtered.txt
         }
+    }
+    catch {
+        Write-Host "Error: $_"
+    }
 }
