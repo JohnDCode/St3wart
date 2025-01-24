183b8c31a705dbef3817a45f64c230f85a3a8072
JohnDCode <johndavidabe101@gmail.com>
2025-01-24 15:30:28 -0800

    Changed baseline on FSB to include all folders

--- FSB/baseline.ps1
+++ FSB/baseline.ps1
@@ -9,7 +9,34 @@
 
 # Get all the files in C:\Windows using powershell
 
-$files = Get-ChildItem C:\Windows\ -Force -File -Recurse -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+$path = ""
+
+
+# Read which folder to baseline on the box
+
+Write-Host "Which folder to baseline?"
+$pathInput = Read-Host -Prompt "[1] Main (Windows) [2] Program Files [3] Program Files (x86) [4] Program Data"
+
+
+# Import csv baselines based on the input (move it to root baselines directory so c binary knows which one to use)
+switch ($pathInput) {
+    "1" {
+		$files = Get-ChildItem "C:\Windows\" -Force -File -Recurse -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+ 	}
+
+    "2" {
+		$files = Get-ChildItem "C:\Program Files\" -Force -File -Recurse -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+	}
+
+    "3" {
+		$files = Get-ChildItem "C:\Program Files (x86)\" -Force -File -Recurse -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+	}
+
+    "4" {
+		$files = Get-ChildItem "C:\ProgramData\" -Force -File -Recurse -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+	}
+}
+
 
 
 # Save the files to a csv (with specific encoding so c binary can read it)
